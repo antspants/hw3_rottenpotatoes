01cd5da71a3b0c7a578ae9055a954017ae72d376
saasbook <saasbook@saasbook.(none)>
2012-06-16 16:24:22 +0200

    Steps to deal with part two and three

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -14,7 +14,11 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.content  is the entire content of the page as a string.
-  flunk "Unimplemented"
+  page.body.should =~ /.*(#{e1}).*(#{e2}).*/m
+end
+
+Then /^I should not see "(.*?)" before "(.*?)"$/ do |e1, e2|
+  page.body.should_not =~ /.*(#{e1}).*(#{e2}).*/m
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -22,7 +26,54 @@ end
 #  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  ratings = rating_list.split(',')
+  case uncheck
+  when 'un' 
+    ratings.each { |rating| step %Q|I uncheck "#{rating}"| }
+  else 
+    ratings.each { |rating| step %Q|I check "#{rating}"| }
+  end
+
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
 end
+
+=begin
+When /^I check the 'PG' and 'R' checkboxes$/ do
+  pending # express the regexp above with the code you wish you had
+end
+=end
+
+
+
+Then /I should see the following films/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    step %Q|I should see "#{movie[:title]}"| 
+  end
+end
+
+Then /^I should not see the following films:$/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    step %Q|I should not see "#{movie[:title]}"| 
+  end
+end
+
+When /^all ratings are unchecked$/ do
+  ratings = Movie.all_ratings
+  ratings.each { |rating| step %Q|I uncheck "#{rating}"| }
+end
+
+When /^all ratings are checked$/ do
+  ratings = Movie.all_ratings
+  ratings.each { |rating| step %Q|I check "#{rating}"| }
+end
+
+Then /^I should see all films$/ do
+  page.should have_css("tbody#movielist tr", count: Movie.all.size) 
+end
+
+Then /^I should not see any films$/ do
+  page.should_not have_css("tbody#movielist tr") ##, count: 0) 
+end
+
