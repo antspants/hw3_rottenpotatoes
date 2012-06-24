6fa54340288012c4bd9db74032712c1dadae873c
saasbook <saasbook@saasbook.(none)>
2012-06-24 19:27:57 +0200

    Added Movie.by_movie_director

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,10 @@ class Movie < ActiveRecord::Base
   def self.all_ratings
     %w(G PG PG-13 NC-17 R)
   end
+
+  class << self
+    def by_movie_director(name)
+      where(director: name)
+    end
+  end
 end
