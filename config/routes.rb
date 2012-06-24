166a4f6f321cd1bdd6e747511c8744c1c1945f67
saasbook <saasbook@saasbook.(none)>
2012-06-24 19:29:55 +0200

    Added new route for new :find_similar_movies functioanlity

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rottenpotatoes::Application.routes.draw do
-  resources :movies
+match 'find_similar_movies/:id' => 'movies#find_similar_movies',
+  via: :get, as: :find_similar_movies
+
+  resources :movies 
+
   # map '/' to be a redirect to '/movies'
   root :to => redirect('/movies')
 end
